a816b0c443f61538a210d5a9c798207325c613c1
francescomascellino <franesco.mascellino@gmail.com>
2023-11-23 16:04:31 +0100

    Delete unused prop

--- src/components/CocktailCard.vue
+++ src/components/CocktailCard.vue
@@ -5,7 +5,6 @@ export default {
 
     props: {
         cocktail: Object,
-        url: String,
     },
 
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,7 +34,7 @@ export default {
 
             <div class="row row-cols-1 row-cols-md-3 d-flex justify-content-center py-3 g-3">
 
-                <CocktailCard :cocktail="cocktail" :url="store.cocktailsUrl" v-for="cocktail in store.cocktails" />
+                <CocktailCard :cocktail="cocktail" v-for="cocktail in store.cocktails" />
 
             </div>
         </div>
