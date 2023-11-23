d1169128cb763970ae2916326881500e5cb3bede
francescomascellino <franesco.mascellino@gmail.com>
2023-11-23 16:57:45 +0100

    Add pagination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,8 +32,27 @@ export default {
             <h1 class="text-center">HOME</h1>
             <h2 class="text-center"><i class="fa-solid fa-martini-glass-citrus"></i> COCKTAILS</h2>
 
+
+            <!-- PAGINATION -->
+            <!-- PUOI AGGIUNGERE v-if="store.currentPage > 1" PER FARLA SPARIRE QUANDO NON CI SONO ABBSTANZA PAGINE -->
+            <nav aria-label="Page navigation" class="my-3">
+
+                <ul class="pagination">
+
+                    <li class="page-item" v-for="link in store.pageLinks" :class="(link.label == store.currentPage ? 'active' : ''),
+                        (link.url == null ? 'disabled' : '')" :key="link.id" aria-current="page">
+
+                        <a class="page-link" href="#home" @click="store.navigate(link.url)" v-html="link.label"></a>
+
+                    </li>
+
+                </ul>
+
+            </nav>
+
             <div class="row row-cols-1 row-cols-md-3 d-flex justify-content-center py-3 g-3">
 
+                <!-- COCKTAIL CARDS -->
                 <CocktailCard :cocktail="cocktail" v-for="cocktail in store.cocktails" />
 
             </div>
