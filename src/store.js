8d6b0e985256ce0545a2d18c9bbf27cb9531af6f
Elisa Boscani <elisabosc90@gmail.com>
2023-11-23 18:37:02 +0100

    create le card dei coktail

--- src/store.js
+++ src/store.js
@@ -1,23 +1,20 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
-import axios from 'axios';
+import axios from "axios";
 
 export const store = reactive({
-    cocktailsUrl: 'http://localhost:8000/api/cocktails',
-    cocktails: null,
+  cocktailsUrl: "http://127.0.0.1:8000/api/cocktails",
+  cocktails: null,
 
-    getCocktails() {
-
-        axios.get(this.cocktailsUrl)
-            .then(response => {
-
-                this.cocktails = response.data.result.data;
-                console.log('Cocktails list:', this.cocktails)
-            })
-            .catch(err => {
-                console.error(err);
-            })
-
-    }
-
-})
+  getCocktails() {
+    axios
+      .get(this.cocktailsUrl)
+      .then((response) => {
+        this.cocktails = response.data.result.data;
+        console.log("Cocktails list:", this.cocktails);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+});
